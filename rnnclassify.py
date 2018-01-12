daa06f644e145f0428aa29f34a9abf96560d74b4
Wei Lee Woon <wlwoon@gmail.com>
2018-01-12 09:15:57 +0400

    tweaks..

--- rnnclassify.py
+++ rnnclassify.py
@@ -1,16 +1,48 @@
+import re
+import tensorflow as tf
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 from sklearn.datasets import load_files
 
 
+# Functions
+#############
+def doc2wv(instr):
+    '''
+    Converts sentences to word vectors
+    (first reduces all to lower case)
+    '''
+    res=[]
+    for word in (tmp.group().strip() for tmp in wordreg.finditer(instr.lower())):
+        try:
+            res.append(worddict[word])
+        except KeyError:
+            pass
+    return np.array(res)
+
+
 # Params
+##########
 trainpath='../Naili_Data/traindata/'
 wordvecpath='/data/glove/glove.6B.50d.txt'
+wordreg=re.compile("\S+")
+nUnits=128
+embedDim=50
 
 # Loading in data
+###################
 dat=load_files(trainpath)
 df=pd.read_csv(wordvecpath,sep="\s",skiprows=[8],names=['word']+['d'+str(tmp) for tmp in range(50)])
 worddict=dict(zip(df.word,df.iloc[:,1:].values))
 
-# Converting to word vectors
-def doc2wv(instr):
-    return -1
+
+# Creating the network
+########################
+x=tf.placeholder(tf.float32,(None,None,embedDim))
+cell=tf.contrib.rnn.BasicLSTMCell(nUnits)
+rnnLayer=tf.nn.dynamic_rnn(cell,x,dtype=tf.float32)
+
+
+# Run it!
+sess=tf.Session()
